7eb508637bd193b86762abd7013e906e16491221
text <270081484@qq.com>
2017-07-04 13:49:11 +0800

    1

--- src/daoI/IBudget.java
+++ src/daoI/IBudget.java
@@ -1,16 +1,18 @@
 package daoI;
 
+import java.util.List;
+
 import model.BeanBudget;
 
 public interface IBudget {
-	//预算�?
+	//预算�?
 	public void addbudget(BeanBudget budget);
 	//添加预算
 	public BeanBudget Searchbudget(int budgetId);
 	//搜索预算
-	public BeanBudget loadbudget(int budgetId);
+	public List<BeanBudget> loadbudget(int budgetId);
 	//搜索预算
-	public BeanBudget loadAllbudget();
+	public List<BeanBudget> loadAllbudget();
 	//搜索预算
 	public void modifrybudget(BeanBudget budget);
 	//修改预算

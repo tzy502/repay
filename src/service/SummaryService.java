49b29f0fca8690f5dd067d3be7708125aa1f0faf
text <270081484@qq.com>
2017-07-07 14:04:56 +0800

    1
    1

---
+++ src/service/SummaryService.java
@@ -0,0 +1,85 @@
+package service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import dao.SummaryDao;
+import model.BeanSummary;
+import serviceI.ISummaryService;
+import util.BaseException;
+@Service
+public class SummaryService implements ISummaryService {
+	@Resource
+	SummaryDao sd=new SummaryDao();
+	@Override
+	public void addSummary(String userId, String company, String projectId, int billCount, float sum, String workerId,
+			String userName, float money, String cardNumber, String manager, String applicationId)
+			throws BaseException {
+		// TODO Auto-generated method stub
+		BeanSummary bs=new BeanSummary();
+		bs.setUserId(userId);
+		bs.setCompany(company);
+		bs.setProjectId(projectId);
+		bs.setBillCount(billCount);
+		bs.setSum(sum);
+		bs.setWorkerId(workerId);
+		bs.setUserName(userName);
+		bs.setMoney(money);
+		bs.setCardNumber(cardNumber);
+		bs.setManager(manager);
+		bs.setApplicationId(applicationId);
+		sd.addSummary(bs);
+	}
+
+	@Override
+	public void modifrySummary(int summaryId, String userId, String company, String projectId, int billCount, float sum,
+			String workerId, String userName, float money, String cardNumber, String manager, String applicationId)
+			throws BaseException {
+		// TODO Auto-generated method stub
+		BeanSummary bs=new BeanSummary();
+		bs.setUserId(userId);
+		bs.setCompany(company);
+		bs.setProjectId(projectId);
+		bs.setBillCount(billCount);
+		bs.setSum(sum);
+		bs.setWorkerId(workerId);
+		bs.setUserName(userName);
+		bs.setMoney(money);
+		bs.setCardNumber(cardNumber);
+		bs.setManager(manager);
+		bs.setApplicationId(applicationId);
+		bs.setSummaryId(summaryId);
+		sd.modifrySummary(bs);
+	}
+
+	@Override
+	public void delSummary(int summaryId) throws BaseException {
+		// TODO Auto-generated method stub
+		sd.DelSummary(searchSummary(summaryId));
+	}
+
+	@Override
+	public BeanSummary searchSummary(int summaryId) throws BaseException {
+		// TODO Auto-generated method stub
+		BeanSummary bs=new BeanSummary();
+		bs=null;
+		bs=sd.SearchSummary(summaryId);
+		if(bs==null){		
+			throw new BaseException("查无数据");
+		}
+		return bs;
+	}
+
+	@Override
+	public List<BeanSummary> loadSummary() throws BaseException {
+		// TODO Auto-generated method stub
+		List<BeanSummary> result=new ArrayList<BeanSummary>();
+		result=sd.loadAllSummary();
+		return result;
+	}
+
+}
---
+++ src/service/TravelUserService.java
@@ -0,0 +1,65 @@
+package service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import dao.TravelUserDao;
+import model.BeanTravelUser;
+import serviceI.ITravelUserService;
+import util.BaseException;
+@Service
+public class TravelUserService implements ITravelUserService {
+	@Resource
+	TravelUserDao tud=new TravelUserDao();
+	@Override
+	public void addTravelUser(int repayId, String userName, String userJob) throws BaseException {
+		// TODO Auto-generated method stub
+		BeanTravelUser btu=new BeanTravelUser();
+		btu.setRepayId(repayId);
+		btu.setUserName(userName);
+		btu.setUserJob(userJob);
+		tud.addTravelUser(btu);
+	}
+
+	@Override
+	public void modifryTravelUser(int travelUserId, int repayId, String userName, String userJob) throws BaseException {
+		// TODO Auto-generated method stub
+		BeanTravelUser btu=new BeanTravelUser();
+		btu.setRepayId(repayId);
+		btu.setUserName(userName);
+		btu.setUserJob(userJob);
+		btu.setTravelUserId(travelUserId);
+		tud.modifryTravelUser(btu);
+	}
+
+	@Override
+	public void delTravelUser(int travelUserId) throws BaseException {
+		// TODO Auto-generated method stub
+		tud.delTravelUser(searchTravelUser(travelUserId));
+	}
+
+	@Override
+	public BeanTravelUser searchTravelUser(int travelUserId) throws BaseException {
+		// TODO Auto-generated method stub
+		BeanTravelUser btu=new BeanTravelUser(); 
+		btu=null;
+		btu=tud.SearchTravelUser(travelUserId);
+		if(btu==null){		
+			throw new BaseException("查无数据");
+		}
+		return btu;
+	}
+
+	@Override
+	public List<BeanTravelUser> loadTravelUser() throws BaseException {
+		// TODO Auto-generated method stub
+		List<BeanTravelUser> result =new ArrayList<BeanTravelUser>();
+		result =tud.loadAllTravelUser();
+		return result;
+	}
+
+}

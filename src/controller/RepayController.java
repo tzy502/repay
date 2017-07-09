d582f3176936ab5d2dd16e1eaba461cadacbc009
text <270081484@qq.com>
2017-07-09 13:09:39 +0800

    1
    1

--- src/controller/RepayController.java
+++ src/controller/RepayController.java
@@ -1,12 +1,20 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import serviceI.*;
+import util.BaseException;
+import model.*;
 
 @Controller
 public class RepayController {
@@ -15,20 +23,38 @@ public class RepayController {
 	@Autowired
 	private ITravelUserService TravelUserService;
 	@Autowired
-	private IRoleService RoleService;
+	private IRepayService RepayService;
 	@RequestMapping(value = "/loadAllrepay.do", produces = "application/json; charset=utf-8") 
 	@ResponseBody
 	public String loadAllrepay() throws JSONException{
-		int repayId;
-		String company;
-		String projectId;
-		String reason;
-		int annex;
-		String annexPath;
-		float checkDays;
-		float checkPlane;
-		float checkTrain;
-		float checkTOther;
+		
+		JSONArray json = new JSONArray();
+		List<BeanRepay> bb=new ArrayList<BeanRepay>();
+		List<BeanTravel> bt=new ArrayList<BeanTravel>();
+		List<BeanTravelUser> btu=new ArrayList<BeanTravelUser>();
+		try {
+			bb=RepayService.loadAllRepay();
+			bt=TravelService.loadTravel();
+			btu=TravelUserService.loadTravelUser();	
+		} catch (BaseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		for(int i=0;i<bb.size();i++){
+			JSONObject jo = new JSONObject();
+			jo.put("repayId", bb.get(i).getRepayId());
+			jo.put("company", bb.get(i).getCompany());
+			jo.put("projectId", bb.get(i).getProjectId());
+			jo.put("reason", bb.get(i).getReason());
+			jo.put("annex", bb.get(i).getAnnex());
+			jo.put("annexPath", bb.get(i).getAnnexPath());
+			jo.put("checkDays", bb.get(i).getCheckDays());
+			jo.put("checkPlane", bb.get(i).getCheckPlane());
+			jo.put("checkTrain", bb.get(i).getCheckTrain());
+			jo.put("checkTOther", bb.get(i).getCheckTOther());
+			
+		}
 		float checkStay;
 		float checkFood;
 		float checkMi;
